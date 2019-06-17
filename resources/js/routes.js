d5fcd0a6521b53fc10bd4436139b0c312daae800
KKadikis <k00kadikis@gmail.com>
2019-06-17 21:47:10 +0300

    Add view for login

--- resources/js/routes.js
+++ resources/js/routes.js
@@ -3,6 +3,7 @@ import VueRouter from 'vue-router';
 
 import Home from '@/js/components/Home.vue';
 import About from '@/js/components/About.vue';
+import Login from '@/js/views/Auth/Login.vue';
 
 Vue.use(VueRouter);
 
@@ -10,14 +11,29 @@ const router = new VueRouter({
     mode: 'history',
     routes : [
         {
-            path: '/',
-            name: 'Home',
-            component: Home
+            path: '/login',
+            name: 'Login',
+            component: Login,
+            meta: {
+                public: true,
+            }
+
         },
         {
             path: '/about',
             name: 'About',
-            component: About
+            component: About,
+            meta: {
+                public: true
+            }
+        },
+        {
+            path: '/',
+            name: 'Home',
+            component: Home,
+            meta: {
+                private: true,
+            }
         }
     ]
 });
